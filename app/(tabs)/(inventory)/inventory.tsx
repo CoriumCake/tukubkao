0dc16f38c4d1a538c67eb8f02e1b90d2123e78ae
CoriumCake <raiflezaza@gmail.com>
2025-04-03 01:14:50 +0700

    remove header, add safeArea

--- app/(tabs)/(inventory)/inventory.tsx
+++ app/(tabs)/(inventory)/inventory.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'expo-router';
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function InventoryScreen() {
   return (
-    <View>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <Text>Inventory Content</Text>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/(profile)/profile.tsx
+++ app/(tabs)/(profile)/profile.tsx
@@ -2,23 +2,26 @@ import { SignOutButton } from '@/components/SignOutButton';
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ProfileScreen() {
   const { user } = useUser();
 
   return (
-    <View>
-      <SignedIn>
-        <View>
-          <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-          <SignOutButton />
-        </View>
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-      </SignedOut>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <SignedIn>
+          <View>
+            <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+            <SignOutButton />
+          </View>
+        </SignedIn>
+        <SignedOut>
+          <Link href="/(auth)/sign-in">
+            <Text>Sign in</Text>
+          </Link>
+        </SignedOut>
+      </View>
+    </SafeAreaView>
   );
 }
