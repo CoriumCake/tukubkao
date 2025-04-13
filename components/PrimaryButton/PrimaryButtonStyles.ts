8495deabf94f38d10fa46ab63170f57c6570a1b7
pitchayanee fern <pitchayaee3376@gmail.com>
2025-04-14 01:07:22 +0700

    add PrimaryButton component and styles

---
+++ components/PrimaryButton/PrimaryButtonStyles.ts
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        marginTop: 15,
+        gap: 5,
+        justifyContent: 'center',
+
+    },
+    buttonContainer: {
+        backgroundColor: "#A5B68D",
+        alignItems: 'center',
+        marginTop: 40,
+        padding: 12,
+        width: '100%',
+        borderRadius: 8,
+    },
+    text: {
+        color: '#FFFFFF',
+        textAlign: 'center',
+        fontSize: 16,
+    },
+    boldText: {
+        fontWeight: 'bold',
+    },
+    contentText: {
+        fontSize: 16,
+    },
+    disabledButton: {
+      opacity: 0.5,
+    },
+});
